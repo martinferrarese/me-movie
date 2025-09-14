c813df36ced29054b261c51b4aef059ffc58b4e0
Martin Ferrarese <ferra@Martins-MacBook-Pro.local>
2025-09-13 22:22:39 -0300

    fix(ci): añade rootDir para jest

--- apps/frontend/jest.config.js
+++ apps/frontend/jest.config.js
@@ -3,8 +3,9 @@ const nextJest = require("next/jest");
 const createJestConfig = nextJest({ dir: __dirname });
 
 module.exports = createJestConfig({
+  rootDir: __dirname,
   testEnvironment: "jest-environment-jsdom",
-  setupFilesAfterEnv: ["./jest.setup.js"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   moduleNameMapper: {
     "\\.(css|less|sass|scss)$": "identity-obj-proxy",
   },
