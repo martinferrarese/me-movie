2a72073d164e12745f19867ac1645eb36e8721bb
Martin Ferrarese <ferra@Martins-MacBook-Pro.local>
2025-09-13 22:20:35 -0300

    fix(ci): ruta relativa jest.setup.js

--- apps/frontend/jest.config.js
+++ apps/frontend/jest.config.js
@@ -3,9 +3,8 @@ const nextJest = require("next/jest");
 const createJestConfig = nextJest({ dir: __dirname });
 
 module.exports = createJestConfig({
-  rootDir: __dirname,
   testEnvironment: "jest-environment-jsdom",
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  setupFilesAfterEnv: ["./jest.setup.js"],
   moduleNameMapper: {
     "\\.(css|less|sass|scss)$": "identity-obj-proxy",
   },
