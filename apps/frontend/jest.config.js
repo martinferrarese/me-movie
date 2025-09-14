641d91db977b9c27f0cc2e122d5329025a6c76df
Martin Ferrarese <ferra@Martins-MacBook-Pro.local>
2025-09-13 22:26:35 -0300

    fix(ci): path.join setup file

--- apps/frontend/jest.config.js
+++ apps/frontend/jest.config.js
@@ -1,11 +1,11 @@
 const nextJest = require("next/jest");
+const path = require("path");
 
-const createJestConfig = nextJest({ dir: __dirname });
+const createJestConfig = nextJest({ dir: "./" });
 
 module.exports = createJestConfig({
-  rootDir: __dirname,
   testEnvironment: "jest-environment-jsdom",
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  setupFilesAfterEnv: [path.join(__dirname, "jest.setup.js")],
   moduleNameMapper: {
     "\\.(css|less|sass|scss)$": "identity-obj-proxy",
   },
