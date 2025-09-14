4f3d5e3dc097bbb1497591b1c08acb89c7884757
Martin Ferrarese <ferra@Martins-MacBook-Pro.local>
2025-09-13 22:11:02 -0300

    test(frontend): usa next/jest para transformar TSX

--- apps/frontend/jest.config.js
+++ apps/frontend/jest.config.js
@@ -1,7 +1,11 @@
-module.exports = {
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({ dir: "./" });
+
+module.exports = createJestConfig({
   testEnvironment: "jest-environment-jsdom",
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   moduleNameMapper: {
-    "\.(css|less|sass|scss)$": "identity-obj-proxy",
+    "\\.(css|less|sass|scss)$": "identity-obj-proxy",
   },
-};
+});
